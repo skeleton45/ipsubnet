34004e09d92dec1dc72bd46c442409a297989fa5
skeleton <54110143+skeleton45@users.noreply.github.com>
2019-08-19 15:24:15 +0700

    Update ipsubnet.py

--- ipsubnet.py
+++ ipsubnet.py
@@ -20,23 +20,23 @@ p = int(input("\n[>>>] Pilihanmu > "))
 
 if p == 1:
 	print ("\n\033[39;1m   [>>>] IP Class Finder [<<<]")
-	a = str(raw_input("\n\033[32;1m[>>>] Masukan IP Adress > "))
+	a = str(raw_input("\n\033[32;1m[>>>] Masukan IP Address > "))
 	
 	if a <= "127.999.999.999":
 		print "\n  \033[33;1m       	[!] KETEMU GAN [!]"
-		print "\n\033[39;1m   [>>>] IP Adress: " , a
+		print "\n\033[39;1m   [>>>] IP Address: " , a
 		print "   [>>>] SUBNETMASK: 255.0.0.0"
 		print "   [>>>] IP Class: A"
 		
 	elif a <= "191.999.999.999":
 		print "\n  \033[33;1m       	[!] KETEMU GAN [!]"
-		print "\n \033[39;1m  [>>>] IP Adress: " , a
+		print "\n \033[39;1m  [>>>] IP Address: " , a
 		print "   [>>>] SUBNETMASK: 255.255.0.0"
 		print "   [>>>] IP Class: B"
 		
 	elif a <= "223.999.999.999":
 		print "\n  \033[33;1m       	[!] KETEMU GAN [!]"
-		print "\n\033[39;1m   [>>>] IP Adress: " , a
+		print "\n\033[39;1m   [>>>] IP Address: " , a
 		print "   [>>>] SUBNETMASK: 255.255.255.0"
 		print "   [>>>] IP Class: C"
 	else: os.system("exit()")
@@ -52,7 +52,7 @@ if p == 2:
 	print ("   *       *    *  *")
 	print ("   *       *    *    ***")
 	print ("\n   \033[33;1m     [x] IP Class & SUBNETMASK Finder Tool [x]")
-	print ("\n\033[39;1m[?] Apasih kegunaan tool/alat ini? Alat ini berfungsi untuk mencari 'Class IP & SUBNETMASK' dengan menggunakan alamat IP atau IP Adress.\n\n[!] Contoh IP Adress: \n   [1] 116.22.177.1\n   [2] 192.168.1.12\n   [3] 222.167.22.11")
+	print ("\n\033[39;1m[?] Apasih kegunaan tool/alat ini? Alat ini berfungsi untuk mencari 'Class IP & SUBNETMASK' dengan menggunakan alamat IP atau IP Address.\n\n[!] Contoh IP Adress: \n   [1] 116.22.177.1\n   [2] 192.168.1.12\n   [3] 222.167.22.11")
 	plh = raw_input("\n[?] Sudah paham? jika sudah ketik y untuk kembali ke menu > ")
 	if plh == "y":
 		os.system("exit()")
